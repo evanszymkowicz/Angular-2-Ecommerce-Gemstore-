ca7c2f51d5ef53f46aa401097f031f43b1e5fba0
Evan Szymkowicz <evan_szymkowicz@Evans-MBP.fios-router.home>
2018-05-25 16:33:50 -0400

    Changes

--- src/app/gem/gem.component.ts
+++ src/app/gem/gem.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { GemModel } from '../gem-model';
-import { CartModel } from '../cart-model';
 
 @Component({
   selector: 'app-gem',
@@ -11,33 +10,6 @@ import { CartModel } from '../cart-model';
 export class GemComponent implements OnInit {
 
   @Input() gem: GemModel;
-  @Input() cart: CartModel;
-
-  addToCart() {
-    this.gem.inventory -= 1;
-    this.cart.totalQuantity += 1;
-    this.cart.totalPrice += this.gem.price;
-
-    var flag = false;
-
-    for (let item of this.cart.items) {
-      if (item.gemid == this.gem.id) {
-        item.quantity += 1;
-        flag = true;
-      }
-    }
-
-    if (flag == false) {
-      this.cart.items.push(
-        {
-          gemid: this.gem.id,
-          name: this.gem.name,
-          unitPrice: this.gem.price,
-          quantity: 1
-        }
-      );
-    };
-  }
 
   constructor() { }
 
